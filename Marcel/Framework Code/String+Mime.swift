1b05a3fabfc6e3bb29da3cb15d2dd6e5fc3be6fa
Ben Gottlieb <ben@standalone.com>
2018-01-01 22:20:55 -0500

    fixed embedded base64 decoding in email header fields

--- Marcel/Framework Code/String+Mime.swift
+++ Marcel/Framework Code/String+Mime.swift
@@ -11,7 +11,8 @@ import Foundation
 extension String {
 	var decodedFromUTF8Wrapping: String {
 		var result = self
-		let utf = "=?utf-8?Q?"
+		let utf = "=?utf-8?q?"
+		let base64 = "=?utf-8?b?"
 		let utfEnd = "?="
 		var done = false
 		var lastUpperBound: Int?
@@ -31,7 +32,20 @@ extension String {
 			result = result.replacingCharacters(in: range.lowerBound..<endRange.upperBound, with: convertedFiller)
 			lastUpperBound = result.distance(from: result.startIndex, to: range.lowerBound) + convertedFiller.count
 		}
-		
+
+		while let range = result.range(of: base64, options: .caseInsensitive), let endRange = result.range(of: utfEnd), range.upperBound < endRange.lowerBound, !done {
+			var upperBound = endRange.lowerBound
+			if let lineBreak = result.range(of: "\n", options: [], range: range.upperBound..<result.endIndex, locale: nil), lineBreak.lowerBound < upperBound {
+				upperBound = lineBreak.lowerBound
+				done = true
+			}
+			let innerRange = range.upperBound..<upperBound
+			let innerChunk = String(result[innerRange]) ?? ""
+			guard let base64Data = Data(base64Encoded: innerChunk), let convertedFiller = String(data: base64Data, encoding: .utf8) else { continue }
+			result = result.replacingCharacters(in: range.lowerBound..<endRange.upperBound, with: convertedFiller)
+			lastUpperBound = result.distance(from: result.startIndex, to: range.lowerBound) + convertedFiller.count
+		}
+
 		if let endLimit = lastUpperBound {
 			result = String(result[..<result.index(result.startIndex, offsetBy: endLimit)])
 		}
